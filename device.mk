d9b761938e5140eba730c99675ed747328a31558
Jackeagle <jackeagle102@gmail.com>
2015-11-04 08:22:29 -0500

    e53g: Add Audio Configs
    - Added Samsung Custom Audio Amp calibration
    
    Change-Id: Id40ecf2bbb36488bac0294fee16610382b70a706
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- device.mk
+++ device.mk
@@ -30,6 +30,12 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
 
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/Tfa9895.cnt:system/etc/Tfa9895.cnt 
+
 # ETC
 PRODUCT_PACKAGES += \
     init.crda.sh \
