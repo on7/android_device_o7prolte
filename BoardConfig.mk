rutvik95/android_device_samsung_e53g

e53g: Reorganize and Cleanup
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

BoardConfig.mk
@@ -1,31 +1,34 @@
-USE_CAMERA_STUB := true
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# inherit from the proprietary version
--include vendor/samsung/e53g/BoardConfigVendor.mk
+LOCAL_PATH := device/samsung/e53g
 
-TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+# Inherit from msm8916-common
+-include device/samsung/msm8916-common/BoardConfigCommon.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := e53g
+# Kernel
+TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_e53g_eur_defconfig
+TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/e53g
 
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 2048
-
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+# Partition Info
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 
+#BOARD_USERDATAIMAGE_PARTITION_SIZE := 
+#BOARD_CACHEIMAGE_PARTITION_SIZE := 
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-TARGET_PREBUILT_KERNEL := device/samsung/e53g/kernel
-
-BOARD_HAS_NO_SELECT_BUTTON := true

on7/android_device_o7prolte

Change-Id: Iafd6cd4f7132596deb9f299406868261331525d6

BoardConfig.mk
@@ -1,31 +1,31 @@
-USE_CAMERA_STUB := true
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# inherit from the proprietary version
--include vendor/samsung/e53g/BoardConfigVendor.mk
+DEVICE_PATH := device/samsung/e53g
 
-TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+# Kernel
+TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_e53g_eur_defconfig
+TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/e53g
 
-TARGET_BOOTLOADER_BOARD_NAME := e53g
-
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 2048
-
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+# Partition Info
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 
+#BOARD_USERDATAIMAGE_PARTITION_SIZE := 
+#BOARD_CACHEIMAGE_PARTITION_SIZE := 
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-TARGET_PREBUILT_KERNEL := device/samsung/e53g/kernel
-
-BOARD_HAS_NO_SELECT_BUTTON := true